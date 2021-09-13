1125803a0fbd16eb68676f3759751be0a6dda62a
Alondrob <alondrob@gmail.com>
2021-09-12 23:21:34 -0400

    Done.

---
+++ build/precache-manifest.956b3ec3c507361f015cb63c5ee1d81c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96bf358192160da8d65f78547571fd7f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1edfce035466a5e55cb0",
+    "url": "/static/js/2.0fcf2331.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.0fcf2331.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8029c08f04141c265d4c",
+    "url": "/static/js/main.8c4a5fd5.chunk.js"
+  },
+  {
+    "revision": "8fa6ce29fee7de43c727",
+    "url": "/static/js/runtime-main.45609115.js"
+  }
+]);
